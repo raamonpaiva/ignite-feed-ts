31badb32f0193e1aac8ccb92523f9c4942a35b7a
Raamon Paiva <raamonpaiva@gmail.com>
2023-07-01 11:17:08 -0400

    Prettier Changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@
 /** 'react' contém o core base do react que pode ser utilizado em varias plataforma,
  * como por exemplo, mobile, tvs, etc.
  */
-import React from 'react'
+import React from "react"
 /** 'react-dom/client' é a biblioteca especifica do react para web */
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
