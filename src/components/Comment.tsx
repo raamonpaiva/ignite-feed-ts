be6c90d6e0a724c9f255915e8fcba78f46d4577f
Raamon Paiva <raamonpaiva@gmail.com>
2023-07-02 16:55:43 -0400

    Added Comments to Post and Removed classLists

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,70 +1,64 @@
-import { ThumbsUp, Trash } from '@phosphor-icons/react'
-import styles from './Comment.module.css'
-import Avatar from './Avatar'
-import { useState } from 'react'
+import { ThumbsUp, Trash } from "@phosphor-icons/react"
+import { useState } from "react"
+import Avatar from "./Avatar"
+import styles from "./Comment.module.css"
 
 /** Tipagem de função segue o exeplo abaixo */
-interface CommentProps{
-    content: string,
-    onDeleteComment: (comment: string) => void;
+interface CommentProps {
+  content: string
+  onDeleteComment: (comment: string) => void
 }
 
-function Comment({content,onDeleteComment}: CommentProps){
+function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0)
 
-    const [likeCount,setLikeCount] = useState(0)
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
 
-    function handleDeleteComment() {
-       onDeleteComment(content)
-    }
+  function handleLikeComment() {
+    /** Forma Simples de Atualizar o valor de likes
+     *  setLikeCount(likeCount + 1)
+     */
 
-    function handleLikeComment(){
-        /** Forma Simples de Atualizar o valor de likes
-         *  setLikeCount(likeCount + 1) 
-        */
+    /** Forma mais padronizada de retornar o like,
+     * sendo possivel pegar o valor mais atualizado dos
+     * likes
+     */
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
 
-        /** Forma mais padronizada de retornar o like,
-         * sendo possivel pegar o valor mais atualizado dos
-         * likes
-         */
-        setLikeCount((state) => {
-            return state +1
-        })
-        
-    }
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="http://github.com/raamonpaiva.png" />
 
-    return(
-        <div className={styles.comment}>
-           <Avatar hasBorder={false} src="http://github.com/raamonpaiva.png"/>
-
-            <div className={styles.commentBox}>
-
-                 <div className={styles.commentContent}>
-
-                    <header className={styles.headerContent}>
-                        <div className={styles.authorAndTime}>
-                            <strong>Raamon Paiva</strong>
-                            <time>Cerca de 1h atrás</time>
-                        </div>
-
-                        <button onClick={handleDeleteComment} title='Deletar Comentário'>
-                            <Trash size={24} />
-                        </button>
-
-                    </header>
-
-                    <p>{content}</p>
-                 </div>
-
-                 <footer>
-                    <button onClick={handleLikeComment}>
-                        <ThumbsUp />
-                        Aplaudir <span>{likeCount}</span>
-
-                    </button>
-                 </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Raamon Paiva</strong>
+              <time>Cerca de 1h atrás</time>
             </div>
+
+            <button onClick={handleDeleteComment} title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
         </div>
-    )
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
 }
 
 export default Comment
